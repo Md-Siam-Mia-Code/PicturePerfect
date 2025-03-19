432c4322b919e9edd6d76cf94c95645643d659ae
Md-Siam-Mia-Code <mdsiammia.main@gmail.com>
2025-03-19 13:02:23 +0600

    Update main.py

--- main.py
+++ main.py
@@ -357,4 +357,4 @@ async def remove_file(data: dict):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=3005)
+    uvicorn.run(app, host="0.0.0.0", port=3015)
